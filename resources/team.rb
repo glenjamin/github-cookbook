a81dc4c4893bd2c576268511a87714f1e7c4a138
Glen Mailer <glenjamin@gmail.com>
2015-10-07 00:46:47 +0100

    team member syncing

--- resources/team.rb
+++ resources/team.rb
@@ -1,6 +1,21 @@
+require 'set'
+
 actions :create
 default_action :create
 
+namelist = lambda do |names|
+  names.all? { |name| name.is_a?(String) }
+end
+
 attribute :name, kind_of: [String, Symbol], required: true, name_attribute: true
 attribute :org, kind_of: [String, Symbol], required: true
+attribute :description, kind_of: String, required: false
 attribute :privacy, equal_to: [:secret, :closed], default: :secret
+attribute :maintainers, kind_of: Array, required: false, default: [],
+                        callbacks: { "should be list of usernames" => namelist }
+attribute :members, kind_of: Array, required: false, default: [],
+                    callbacks: { "should be list of usernames" => namelist }
+
+def all_users
+  (maintainers + members).to_set
+end
--- test/fixtures/cookbooks/github-test/recipes/default.rb
+++ test/fixtures/cookbooks/github-test/recipes/default.rb
@@ -1,16 +1,21 @@
 github_org node['github-test']['org'] do
-  admins ['glenjamin']
-  members ['sky-glenjamin']
+  admins %w(glenjamin)
+  members %w(glenjamin1 glenjamin2 glenjamin3 glenjamin4)
 end
 
 github_team "america" do
   org node['github-test']['org']
+  description "bad pun"
   privacy :closed
+  maintainers %w(glenjamin3)
+  members %w(glenjamin glenjamin1 glenjamin2)
 end
 
 github_team "player" do
   org node['github-test']['org']
   privacy :secret
+  maintainers %w(glenjamin1)
+  members %w(glenjamin3 glenjamin4)
 end
 
 github_repo :test do
